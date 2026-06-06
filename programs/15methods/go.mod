e87feb019c1a888c18eb08f4f2d23832e0b26bf4
ankit <ankitpanchal41106@gmail.com>
2026-06-06 18:46:43 +0530

    add methods

---
+++ programs/15methods/go.mod
@@ -0,0 +1,3 @@
+module myMethods
+
+go 1.26.3
---
+++ programs/15methods/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	ankit:=User{"ankit",20,"ap@.com",true}
+	ankit.GetStatus()
+	fmt.Println(ankit.Email)
+	ankit.NewEmail()
+	fmt.Println(ankit.Email)
+}
+
+type User struct {
+	Name   string
+	Age    int
+	Email  string
+	Status bool
+}
+
+// methods are functions associated with a Struct
+
+func (u User) GetStatus() { // here we are getting only the copy of the calling Struct variable not the actual address thus any changes happen inside the method to the variable doesnt effect the original var
+	fmt.Println(u.Status)
+}
+
+func (u User) NewEmail(){
+	u.Email = "test@.com"
+	fmt.Println("new email of user is:",u.Email)
+}
